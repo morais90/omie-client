f096d9706458210c11790b53737e963f2592c50f
Willian Morais <williandmorais@gmail.com>
2024-06-22 15:08:22 -0300

    chore: catch and re-raise request exceptions on OmieClient

--- omie_client/client.py
+++ omie_client/client.py
@@ -2,6 +2,7 @@ import httpx
 from pydantic import HttpUrl
 
 from omie_client.accounts_payable import AccountsPayable
+from omie_client.exceptions import OmieBlockedError, OmieRequestError
 
 
 class OmieClient:
@@ -14,9 +15,7 @@ class OmieClient:
     ):
         self._app_key = app_key
         self._app_secret = app_secret
-        self._client = httpx.Client(
-            base_url=api_base_url, timeout=default_timeout, http2=True
-        )
+        self._client = httpx.Client(base_url=api_base_url, timeout=default_timeout, http2=True)
 
     def request(self, url: str, data: dict):
         authenticated_data = {
@@ -24,10 +23,24 @@ class OmieClient:
             "app_secret": self._app_secret,
             **data,
         }
-        response = self._client.post(url, json=authenticated_data)
-        response.raise_for_status()
 
-        return response.json()
+        try:
+            response = self._client.post(url, json=authenticated_data)
+            response.raise_for_status()
+            return response.json()
+
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 500:
+                error_data = e.response.json()
+
+                raise OmieRequestError(
+                    status_code=e.response.status_code,
+                    code=error_data.get("code", "SOAP-ENV:Unknown-error"),
+                    message=error_data.get("faultstring", "Unknown error"),
+                )
+
+            elif e.response.status_code == 403:
+                raise OmieBlockedError
 
     @property
     def accounts_payable(self):
